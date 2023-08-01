be696e650740b1d39f7ca3a4e178d34ba7709479
sagar s Devaraddi <129572023+sagarsdevaraddi@users.noreply.github.com>
2023-08-01 10:20:30 +0530

    Add files via upload
    java Ascii program

---
+++ Ascii.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+class Ascii
+{
+	public static void main(String[] args) 
+	{
+		int i,n;
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+		n = sc.nextInt();
+
+		for(i=0;i<=n;i++) {
+			System.out.println(i+" "+(char)i);
+		}
+	}
+}
